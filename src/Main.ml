2cfd43bf53d7990986e2dfa836ca4e54f94bc25b
Ipsedo <7268@samu>
2017-12-17 00:22:42 +0100

    changé encore 2/3 trucs

--- src/Main.ml
+++ src/Main.ml
@@ -1,39 +1,50 @@
 open OpenMnist
 open MultiClassPerceptron
 
+let usage = "usage: ./Main.native [options]"
+
+let mnist = ref true
+let nb_training_mnist_img = ref 6000
+
+let spec =
+  [
+    "-mnist", Arg.Tuple [Arg.Set_int nb_training_mnist_img; Arg.Set mnist], "  on mnist training set"
+  ]
+
+let parse_args =
+  Arg.parse spec (fun a -> ()) usage
+
 let train_mnist () =
   Random.init (int_of_float (Unix.gettimeofday ()));
-  let img_set = mk_train_set 60000 in
+  let img_set, info = mk_train_set !nb_training_mnist_img in
   let norm_set = normalize_set_mnist img_set in
   let norm_set = randomize_order norm_set in
+  print_info info;
   let perc = init_perceptron 10 (28 * 28) in
   for i = 0 to 20 do
     let nb_err = epoch perc norm_set in
-    Printf.printf "Error(s) : %d, pas %f\n" nb_err perc.pas;
+    Printf.printf "Error(s) : %d, pas %f\n%!" nb_err perc.pas;
   done;
   perc
 
 let test_mnist perc =
-  let img_set = mk_test_set 10000 in
+  let img_set, info = mk_test_set 10000 in
   let norm_set = normalize_set_mnist img_set in
   let norm_set = randomize_order norm_set in
   (* On test le modèle *)
-  Printf.printf "Test perceptron (y / n) ? ";
-  let will_test = ref (read_line () = "y") in
-  Printf.printf "\n";
-  while !will_test do
+  let separator = String.make 56 '=' in
+  Printf.printf "%s\nTest perceptron (y) ? " separator;
+  while read_line () = "y" do
     let random_index = Random.int (Array.length norm_set) in
     Printf.printf "What is this number ? :\n";
     print_data norm_set.(random_index);
     let test_img = norm_set.(random_index) in
     Printf.printf "Perceptron predict %d, real : %d\n" (predict perc test_img) test_img.label;
-    Printf.printf "Test perceptron (y / n) ? ";
-    will_test := read_line () = "y";
-    Printf.printf "\n";
+    Printf.printf "%s\nTest perceptron (y) ? " separator;
   done
 
 let test_all_mnist perc =
-  let img_set = mk_test_set 10000 in
+  let img_set, info = mk_test_set 10000 in
   let norm_set = normalize_set_mnist img_set in
   let norm_set = randomize_order norm_set in
   let aux acc img =
@@ -45,6 +56,8 @@ let test_all_mnist perc =
   Printf.printf "Error(s) : %d\n" nb_err
 
 let () =
-  let perc = train_mnist () in
-  test_mnist perc;
-  test_all_mnist perc
+  if !mnist then begin
+    let perc = train_mnist () in
+    test_mnist perc;
+    test_all_mnist perc
+  end;
--- src/OpenMnist.ml
+++ src/OpenMnist.ml
@@ -1,6 +1,16 @@
 type image = { pixels : int array; label : int }
 
-type set = image array
+type set_info =
+  {
+    magic_number_img : int;
+    magic_number_label : int;
+    number_of_img : int;
+    number_of_label : int;
+    img_resolution : int * int;
+
+  }
+
+type set = image array * set_info
 
 let res_folder = "./res/"
 
@@ -28,28 +38,39 @@ let mk_train_set ic_train ic_label nb_img =
   done;
   set
 
+let print_info info =
+  Printf.printf "Image file\n";
+  Printf.printf "Magic number : %d\n" info.magic_number_img;
+  Printf.printf "Number of image : %d\n" info.number_of_img;
+  Printf.printf "Images are %d * %d\n" (fst info.img_resolution) (snd info.img_resolution);
+  Printf.printf "Label file:\n";
+  Printf.printf "Magic number : %d\n" info.magic_number_label;
+  Printf.printf "Number of label : %d%!\n" info.number_of_label
+
 let mk_set nb_img images_file labels_file =
   let ic_train = open_in (res_folder ^ images_file) in
-  Printf.printf "Image file\n";
-  Printf.printf "Magic number : %d\n" (input_binary_int ic_train);
-  Printf.printf "Number of image : %d\n" (input_binary_int ic_train);
-  Printf.printf "Images are %d * %d\n" (input_binary_int ic_train) (input_binary_int ic_train);
+  let magic_number_img = input_binary_int ic_train in
+  let number_of_img = input_binary_int ic_train in
+  let img_resolution = (input_binary_int ic_train), (input_binary_int ic_train) in
   let ic_label = open_in (res_folder ^ labels_file) in
-  Printf.printf "Label file:\n";
-  Printf.printf "Magic number : %d\n" (input_binary_int ic_label);
-  Printf.printf "Number of label : %d\n" (input_binary_int ic_label);
+  let magic_number_label = input_binary_int ic_label in
+  let number_of_label = input_binary_int ic_label in
   let train_set = mk_train_set ic_train ic_label nb_img in
   close_in ic_train;
   close_in ic_label;
-  Printf.printf "\n";
-  train_set
+  train_set,
+  {
+    magic_number_img = magic_number_img;
+    number_of_img = number_of_img;
+    img_resolution = img_resolution;
+    magic_number_label = magic_number_label;
+    number_of_label = number_of_label
+  }
 
 let mk_train_set nb_img =
-  Printf.printf "Train set :\n";
   mk_set nb_img "train-images.idx3-ubyte" "train-labels.idx1-ubyte"
 
 let mk_test_set nb_img =
-  Printf.printf "Test set :\n";
   mk_set nb_img "t10k-images.idx3-ubyte" "t10k-labels.idx1-ubyte"
 
 open MultiClassPerceptron
